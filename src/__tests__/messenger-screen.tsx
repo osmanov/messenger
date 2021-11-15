48b440be70f30cfdb9bc941b07a5835cab5aa56a
osmanov <rhenium.osmium@gmail.com>
2021-11-15 22:07:55 +0300

    add test render app with selected chat

--- src/__tests__/messenger-screen.tsx
+++ src/__tests__/messenger-screen.tsx
@@ -7,8 +7,13 @@ import {
 
 import * as React from 'react';
 import {fetchGetUsers, IUser} from '../utils/users';
+import userEvent from '@testing-library/user-event';
+
 const waitForLoadingToFinish = () =>
-  waitForElementToBeRemoved(() => [...screen.queryAllByText(/loading/i)]);
+  waitForElementToBeRemoved(() => [
+    ...screen.queryAllByText(/loading/i),
+    ...screen.queryAllByText(/loading.../i),
+  ]);
 type TRenderMessengerScreen = {
   users?: IUser[];
 };
@@ -23,7 +28,7 @@ async function renderMessengerScreen({users}: TRenderMessengerScreen = {}) {
   };
 }
 
-test('renders an app with users', async () => {
+test('renders an app with sidebar users', async () => {
   const {users} = await renderMessengerScreen();
   await waitForLoadingToFinish();
   users.forEach(user => {
@@ -38,4 +43,14 @@ test('renders an app with no selected chat', async () => {
   );
   expect(noActiveUserMessage).toBeInTheDocument();
 });
-// TODO next render app with selected chat
+
+test('renders an app with selected chat', async () => {
+  const {users} = await renderMessengerScreen();
+  const userFirstItem = screen.getByText(`${users?.[0].name}`);
+  userEvent.click(userFirstItem);
+  await waitForLoadingToFinish();
+  const activeUserNoMessage = screen.getByText(
+    `Say Hi to ${users?.[0].name}👋`,
+  );
+  expect(activeUserNoMessage).toBeInTheDocument();
+});
